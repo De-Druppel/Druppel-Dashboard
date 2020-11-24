53fecda756545021b581a2ad6d23f1668a185ee8
Peter Askey <peter_g_askey@live.co.uk>
2020-11-24 12:50:54 +0100

    removed dummy data for final version of branch

--- src/store/index.js
+++ src/store/index.js
@@ -6,12 +6,7 @@ import * as actions from './actions'
 Vue.use(Vuex)
 
 const state = {
-    Plants: [
-        {espId: 12321, status:true, moisture:"00.00"},
-        {espId: 12322, status:false, moisture:"00.00"},
-        {espId: 12323, status:true, moisture:"00.00"},
-        {espId: 12324, status:false, moisture:"00.00"}
-    ]
+    Plants: []
 }
 
 export default new Vuex.Store({
